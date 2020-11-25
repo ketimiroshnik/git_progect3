673120ead85ead908d0182f0d901c3440fe718ca
Keti <keti.miroshnik@gmail.com>
2020-11-25 20:23:28 +0300

    Эспрессо

---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+import sqlite3
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.load_table()
+
+    def load_table(self):
+        self.tableWidget.clear()
+        cur = self.con.cursor()
+        result = cur.execute("SELECT * FROM coffee").fetchall()
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        titles = [description[0] for description in cur.description]
+        self.tableWidget.setHorizontalHeaderLabels(titles)
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Window()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
